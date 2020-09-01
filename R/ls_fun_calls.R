59575bd06bbaaa260307ae3086687eb614bbb5d6
trestletech <github@trestletech.com>
2020-09-01 17:02:07 -0500

    Add package namespace for function calls

--- R/ls_fun_calls.R
+++ R/ls_fun_calls.R
@@ -11,7 +11,26 @@ ls_fun_calls <- function(x) {
     c(ls_fun_calls(formals(x)), ls_fun_calls(body(x)))
   }
   else if (is.call(x)) {
+    pkg <- NA
     fname <- as.character(x[[1]])
-    c(list(fname), unlist(lapply(x[-1], ls_fun_calls), use.names = FALSE, recursive = FALSE))
+    ex <- eval(x[[1]])
+    if (is.primitive(ex)){
+      pkg <- "base"
+    } else {
+      ee <- environment(ex)
+      if (isBaseNamespace(ee)){
+        # Not allowed to getNamespaceInfo from base
+        pkg <- "base"
+      } else {
+        pkg <- paste0(getNamespaceInfo(ee, "spec")[["name"]])
+      }
+    }
+    ch <- unlist(lapply(x[-1], ls_fun_calls), use.names = TRUE, recursive = FALSE)
+    r <- list(c(pkg = pkg, fun = fname))
+    if (is.null(ch)){
+      r
+    } else {
+      append(r, ch)
+    }
   }
 }
